fe34f656aeffcc3debeee156f29efd541a53b74c
Arpit27kr <arpit27kr@gmail.com>
2023-10-15 01:25:26 +0530

    Project of major

---
+++ ProductService/src/main/java/com/major/ProductService/controller/ProductController.java
@@ -0,0 +1,46 @@
+package com.major.ProductService.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.major.ProductService.model.ProductRequest;
+import com.major.ProductService.model.ProductResponse;
+import com.major.ProductService.service.ProductService;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+	
+	@Autowired
+	private ProductService productService;
+	
+	@PostMapping
+	public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest){
+		
+		Long productid=productService.addproduct(productRequest);
+		
+		return new ResponseEntity<>(productid,HttpStatus.CREATED);
+		
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ProductResponse>getProductId(@PathVariable("id") long productId)  
+	{
+		ProductResponse productResponse =productService.getProductById(productId);
+		
+		return new ResponseEntity<>(productResponse,HttpStatus.OK);
+		
+	}
+	
+
+	
+	
+	
+}
---
+++ ProductService/src/main/java/com/major/ProductService/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.major.ProductService.service;
+
+import com.major.ProductService.model.ProductRequest;
+import com.major.ProductService.model.ProductResponse;
+
+public interface ProductService {
+
+	Long addproduct(ProductRequest productRequest);
+
+	ProductResponse getProductById(long productId);
+
+
+
+
+}
